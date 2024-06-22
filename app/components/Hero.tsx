d77b5d2c651177c4d76a9dab564a7627dc71a894
Chamika1016 <rclurathnaweera@std.appsc.sab.ac.lk>
2024-06-22 17:22:56 +0530

    Adding hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import BlurArrow from "../../public/blue-button.svg";
+import Image from "next/image";
+import Gradient from "../../public/Gradient.svg";
+import HeroImage from "../../public/Image.svg";
+import Google from "../../public/Google.svg";
+import Slack from "../../public/Slack.svg";
+import Truspilot from "../../public/Trustpilot.svg";
+import Cnn from "../../public/CNN.svg";
+import Clutch from "../../public/Clutch.svg";
+function Hero() {
+  return (
+    <div className="pt-4 lg:pt-10">
+      <div className="px-[20px] lg:px-[280px]">
+        <h1 className="text-center text-[32px] leading-[40px] font-medium text-[#36485c] lg:text-[64px] lg:leading-[72px]  ">
+          Start Monitoring Your Website Like a Pro..
+        </h1>
+        <p className="text-center pt-6 text-[#36485c] lg:text-[18px] lg:leading-[2]">
+          Get a bird's eye view with our customizable dashboard.Stay on top of
+          things!. Revamp your work process with our game-changing feature.
+          Boost Productivity and efficiency!.
+        </p>
+
+        <div className="flex w-full pt-8 justify-center gap-x-6">
+          <button className="bg-[#4328eb] w-1/2 py-4 px-8 text-white rounded-[4px] lg:w-fit">
+            Try for free
+          </button>
+          <button className="w-1/2 text-[#4328eb] flex items-center justify-center gap-x-2 lg:w-fit">
+            View Pricing{" "}
+            <span>
+              <Image src={BlurArrow} alt="learn more" />
+            </span>
+          </button>
+        </div>
+      </div>
+
+      <div className="relative flex h-full w-full justify-center">
+        <Image
+          src={Gradient}
+          alt="gradient"
+          className="min-h-[500px] w-full object-cover lg:h-auto"
+        />
+        <div className="absolute bottom-5 flex w-full flex-col items-center">
+          <Image
+            src={HeroImage}
+            alt="hero image"
+            className="-ml-4 h-[310px] sm:-mb-2 sm:h-[400px] lg:-mb-28 lg:h-auto xl:w-[70%]"
+          />
+          <div className="flex w-full flex-col items-center lg:container lg:flex-row lg:justify-between lg:px-20">
+            <p className="text-[#ffffff] text-center lg:text-[18px] ">
+              Trusted by these companies
+            </p>
+            <div className="grid grid-cols-3 items-center justify-center justify-items-center px-[20px] align-middle lg:grid-cols-5 ">
+              <Image src={Google} alt="" />
+              <Image src={Slack} alt="" />
+              <Image src={Truspilot} alt="" />
+              <Image src={Cnn} alt="" />
+              <Image src={Clutch} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
